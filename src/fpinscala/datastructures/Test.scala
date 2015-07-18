059806b2daed4ba05603faead7b4542b9f4166f9
Carlos Rodríguez <cerodriguezl@gmail.com>
2015-07-18 18:30:59 -0500

    Data structures - Implementation of List.filter using flatMap

--- src/fpinscala/datastructures/Test.scala
+++ src/fpinscala/datastructures/Test.scala
@@ -2,7 +2,8 @@ package fpinscala.datastructures
 
 object Test {
     def main(args: Array[String]) {
-        val l = List.flatMap(List(1,2,3))(i => List(i,i))
+        val a = List(1, 2, 3, 4, 5, 6)
+        val l = List.filterViaFlatMap(a)(_ % 2 == 0)
         println(l)
     }
 }
