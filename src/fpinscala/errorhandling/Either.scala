293f54b30ab1898721888cce3eb6bbd24a182927
Carlos Rodríguez <carlos@voicebunny.com>
2015-07-29 17:07:42 -0500

    Error handling - Implementation of Either.map2

--- src/fpinscala/errorhandling/Either.scala
+++ src/fpinscala/errorhandling/Either.scala
@@ -19,7 +19,8 @@ sealed trait Either[+E, +A] {
         case Left(_)  => b
     }
 
-    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = sys.error("todo")
+    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+        for { a <- this; b1 <- b } yield f(a, b1)
 
 }
 
