6a22b3681689b5775250741e48197d15153b3fd7
Carlos Rodríguez <carlos@voicebunny.com>
2015-08-19 18:53:53 +0900

    State - Implementation of State.map2

--- src/fpinscala/state/State.scala
+++ src/fpinscala/state/State.scala
@@ -68,4 +68,10 @@ object RNG {
         go(count, List(), rng)
     }
 
+    def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+        rng => {
+            val (a, rng1) = ra(rng)
+            val (b, rng2) = rb(rng1)
+            (f(a, b), rng2)
+        }
 }
