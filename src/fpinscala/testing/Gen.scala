4806e30cd747486939f36f5051bdb2f989a9c692
Carlos Rodríguez <cerodriguezl@gmail.com>
2015-11-01 17:00:40 -0500

    Testing - Implementation of Prop.&&

---
+++ src/fpinscala/testing/Gen.scala
@@ -0,0 +1,34 @@
+package fpinscala.testing
+
+import fpinscala.laziness.Stream
+import fpinscala.state._
+import fpinscala.parallelism._
+import fpinscala.parallelism.Par.Par
+import Gen._
+import Prop._
+import java.util.concurrent.{ Executors, ExecutorService }
+
+trait Prop {
+    def check: Boolean
+
+    def &&(p: Prop): Prop = new Prop {
+        def check = Prop.this.check && p.check
+    }
+}
+
+object Prop {
+    def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+}
+
+object Gen {
+    def unit[A](a: => A): Gen[A] = ???
+}
+
+trait Gen[A] {
+    def map[A, B](f: A => B): Gen[B] = ???
+    def flatMap[A, B](f: A => Gen[B]): Gen[B] = ???
+}
+
+trait SGen[+A] {
+
+}
